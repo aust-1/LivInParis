d86df9c915ed16678d672b5f845d42a302e3dc05
proxy-1 <183744404+proxy-1@user.noreply.github.com>
2025-04-02 16:33:42 +0200

    start of implementation

--- src/LivinParis/data/Repositories/Implementations/AccountRepository.cs
+++ src/LivinParis/data/Repositories/Implementations/AccountRepository.cs
@@ -1,1 +1,29 @@
-namespace LivinParis.Models.Order;
+namespace LivinParis.Data;
+using MySql.Data.MySqlClient;
+
+public class AccountRepository : IAccount
+{
+    private static MySqlConnection? conn;
+
+    public static bool InitConnection()
+    {
+        try
+        {
+            string connection = "SERVER=localhost;PORT=3306;DATABASE=nom_database;UID=eliottfrancois;PASSWORD=PSI";
+            MySqlConnection conn = new MySqlConnection(connection);
+            conn.Open();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+        
+    }
+
+    public static void CloseConnection()
+    {
+        conn?.Close();
+    }
+    
+}
