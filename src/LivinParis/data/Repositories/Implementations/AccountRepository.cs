0946e20250ed61e3d35b2242329e27557cc64a4b
proxy-1 <183744404+proxy-1@user.noreply.github.com>
2025-04-02 19:46:15 +0200

    accountrepository updated to add CRUD functionnality

--- src/LivinParis/data/Repositories/Implementations/AccountRepository.cs
+++ src/LivinParis/data/Repositories/Implementations/AccountRepository.cs
@@ -1,29 +1,106 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LivinParis.Data;
 using MySql.Data.MySqlClient;
 
-public class AccountRepository : IAccount
+public static class AccountRepository : IAccount
 {
-    private static MySqlConnection? conn;
+    private static MySqlConnection? s_connection;
 
     public static bool InitConnection()
     {
         try
         {
-            string connection = "SERVER=localhost;PORT=3306;DATABASE=nom_database;UID=eliottfrancois;PASSWORD=PSI";
-            MySqlConnection conn = new MySqlConnection(connection);
-            conn.Open();
+            string connection = "SERVER=localhost;PORT=3306;DATABASE=PSI;UID=eliottfrancois;PASSWORD=PSI";
+            s_connection = new MySqlConnection(connection);
             return true;
         }
-        catch (Exception ex)
+        catch (Exception)
         {
             return false;
         }
-        
+    }
+
+    public static void OpenConnection()
+    {
+        s_connection!.Open();
     }
 
     public static void CloseConnection()
     {
-        conn?.Close();
+        s_connection!.Close();
     }
+
+    ///CRUD
+
+    public static void CreateAccount(string accountId, string email,string password)
+    {
+        OpenConnection();
+        using var command = new MySqlCommand();
+        command.CommandText = "INSERT INTO account VALUES (@a,@e, @p)";
+        command.Parameters.AddWithValue("@e", email);
+        command.Parameters.AddWithValue("@a", accountId);
+        command.Parameters.AddWithValue("@p", password);
+        command.ExecuteNonQuery();
+        CloseConnection();
+    }
+
+    public static List<List<string>> GetAccounts(int limit)
+    {
+        OpenConnection();
+        using var command = new MySqlCommand();
+        command.CommandText = "SELECT * FROM account LIMIT @l";
+        command.Parameters.AddWithValue("@l", limit);
+        
+        using var reader = command.ExecuteReader();
+        List<List<string>> accounts = [];
+        while (reader.Read())
+        {
+            List<string> account = [];
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                string value = reader[i]?.ToString() ?? string.Empty;
+                account.Add(value);
+            }
+            accounts.Add(account);
+        }
+        return accounts;
+
+    }
+
+    public static void UpdateEmail(string accountId, string email)
+    {
+        OpenConnection();
+        using var command = new MySqlCommand();
+        command.CommandText = "UPDATE account SET email = @e WHERE id = @a";
+        command.Parameters.AddWithValue("@e", email);
+        command.Parameters.AddWithValue("@a", accountId);
+        command.ExecuteNonQuery();
+        CloseConnection();
+    }
+
+    public static void UpdatePassword(string accountId, string password)
+    {
+        OpenConnection();
+        using var command = new MySqlCommand();
+        command.CommandText = "UPDATE account SET password = @p WHERE id = @a";
+        command.Parameters.AddWithValue("@e", password);
+        command.Parameters.AddWithValue("@p", accountId);
+        command.ExecuteNonQuery();
+        CloseConnection();
+    }
+
+    public static void DeleteAccount(string accountId)
+    {
+        OpenConnection();
+        using var command = new MySqlCommand();
+        command.CommandText = "DELETE FROM account WHERE account_id = @a";
+        command.Parameters.AddWithValue("@a", accountId);
+        command.ExecuteNonQuery();
+        CloseConnection();
+    }
+    
+    
+    
     
 }
