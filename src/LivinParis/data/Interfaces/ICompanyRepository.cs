2a73a65dcf3e60cf1246f86a56f7feb7bd83f127
austin <eliottroussille@gmail.com>
2025-05-05 02:42:25 +0200

    📦 delete all for rebuild

--- src/LivinParis/data/Interfaces/ICompanyRepository.cs
+++
@@ -1,6 +0,0 @@
-namespace LivInParisRoussilleTeynier.Data.Interfaces;
-
-/// <summary>
-/// Provides methods for managing company customer accounts.
-/// </summary>
-public interface ICompanyRepository { }
--- src/LivinParis/data/Interfaces/IIndividualRepository.cs
+++
@@ -1,6 +0,0 @@
-namespace LivInParisRoussilleTeynier.Data.Interfaces;
-
-/// <summary>
-/// Provides methods for managing individual customer profiles.
-/// </summary>
-public interface IIndividualRepository : IRepository<Individual> { }
